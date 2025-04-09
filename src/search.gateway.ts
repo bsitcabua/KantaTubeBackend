a3bfb327f06c676a31305390f553ef5213fbf336
EM Cabua <bsitcabua@gmail.com>
2025-04-09 16:33:18 +0800

    test updates

--- src/search.gateway.ts
+++ src/search.gateway.ts
@@ -11,7 +11,7 @@ import { Server, Socket } from 'socket.io';
 const isProd = process.env.NODE_ENV === 'production';
 
 @Injectable()
-@WebSocketGateway(4202,{ // remove for production 4202
+@WebSocketGateway(isProd ? null : 4202,{ // remove for production 4202
 // @WebSocketGateway({
   cors: {
     origin: '*', // Change this to match your frontend domain
